c672f965177f2a19de3e881bb4443704a862679f
fr1sk <fr1sk@live.com>
2017-12-30 14:47:58 +0100

    🔓

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
 require('dotenv').config()
+const express = require('express');
 const request = require('request');
+const mongoose = require('mongoose');
 
-request.post({
-    url:     process.env.UNLOCK_URI,
-    form:    { UNLOCK2: "on" }},
-function(err,res,body){
-  if(err){
-      console.log("=== ERROR ===");
-      console.log(err);
-      return;
-  }
-  if(res.statusCode !== 200 ){
-      console.log(res)
-      console.log(res.statusCode);
-      return;
-  }
-  console.log("=== SUCCESS ===")
-  console.log(res.statusCode);
+mongoose.connect(process.env.MONGO_URI);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('DB connection established!');
 });
+
+//var app = express();
+var Schema = mongoose.Schema;
+var ipSchema = new Schema({
+    "ip": String
+});
+
+var IP = mongoose.model('ip', ipSchema);
+
+let result = IP.findById(process.env.id, (err, res) => {
+        console.log(res.ip);
+        let url = 'http://'+res.ip;
+        request.post({
+            url:     url,
+            form:    { UNLOCK2: "on" }},
+        function(err,res,body){
+          if(err){
+              console.log("=== ERROR ===");
+              console.log(err);
+              return;
+          }
+          if(res.statusCode !== 200 ){
+              console.log(res)
+              console.log(res.statusCode);
+              return;
+          }
+          console.log("=== SUCCESS ===")
+          console.log(res.statusCode);
+        });
+});
+
